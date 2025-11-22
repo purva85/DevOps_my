ba3e9acad6f079954014d14c85ead08bdd67c2c5
purva12 <143676523+purva85@users.noreply.github.com>
2025-11-22 14:40:35 +0530

    bas

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,9 +71,9 @@ resource "aws_instance" "my_ec2" {
 }
 
 
-output "public_ip" {
-  value = aws_instance.my_ec2.public_ip
-}
+//output "public_ip" {
+//  value = aws_instance.my_ec2.public_ip
+//}
 
 
 //provider "aws" {
