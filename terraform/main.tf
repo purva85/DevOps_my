b7cffa8d56af1d7fb50b778c57f931a7fe34a5c5
purva12 <143676523+purva85@users.noreply.github.com>
2025-11-22 13:07:00 +0530

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,89 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.route.id
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow_ssh_tomcat"
+  description = "Allow SSH & Tomcat"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0a716d3f3b16d290c" 
-  instance_type = "t3.micro"
-  availability_zone = "eu-north-1a"
-  key_name      = "ipat-eunorth1"
+  ami                    = "ami-0a716d3f3b16d290c" # Replace if different
+  instance_type          = "t3.micro"
+  key_name               = "ipat-eunorth1"        # Replace with your key
+  subnet_id              = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
     Name = "firststsproject"
   }
 }
+
+output "public_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
+
+//provider "aws" {
+ // region = "eu-north-1"
+//}
+
+//resource "aws_instance" "my_ec2" {
+//  ami           = "ami-0a716d3f3b16d290c" 
+//  instance_type = "t3.micro"
+//  availability_zone = "eu-north-1a"
+//  key_name      = "ipat-eunorth1"
+
+//  tags = {
+//    Name = "firststsproject"
+ // }
+//}
